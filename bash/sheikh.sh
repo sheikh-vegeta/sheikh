e301c71d441113250fa635a753c1ce6de9dcc3b8
Rekt Developer <isaac.likhon@yahoo.com>
2025-05-26 16:16:18 +0600

    Create sheikh.sh

---
+++ bash/sheikh.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e -E
+
+GEMINI_API_KEY="$GEMINI_API_KEY"
+MODEL_ID="gemini-2.5-pro-preview-05-06"
+GENERATE_CONTENT_API="streamGenerateContent"
+
+cat << EOF > request.json
+{
+    "contents": [
+      {
+        "role": "user",
+        "parts": [
+          {
+            "text": "INSERT_INPUT_HERE"
+          },
+        ]
+      },
+    ],
+    "generationConfig": {
+      "responseMimeType": "text/plain",
+    },
+}
+EOF
+
+curl \
+-X POST \
+-H "Content-Type: application/json" \
+"https://generativelanguage.googleapis.com/v1beta/models/${MODEL_ID}:${GENERATE_CONTENT_API}?key=${GEMINI_API_KEY}" -d '@request.json'
